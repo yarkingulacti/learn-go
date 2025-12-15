bb64ac1bfa057773e12aa880feefb91bcc6911a2
Yarkın <yarkingulacti@outlook.com>
2025-12-15 16:11:46 +0300

    Add initial implementation of main package and go.mod file

---
+++ main.go
@@ -0,0 +1,19 @@
+package main // special name means entry point
+
+// "strings"
+
+// "errors"
+
+// "unicode/utf8"
+
+func main() {
+	variables()
+	conditions()
+	functions()
+	arrays()
+	forLoop()
+	maps()
+	slices()
+	runes()
+	structs()
+}
