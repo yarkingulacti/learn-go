0d97d6a9b6f391d1349f58c494006e39800cb3b2
Yarkın <yarkingulacti@outlook.com>
2025-12-15 19:50:36 +0300

    Add animal types demonstration using generics and enhance output formatting

--- 14-generics.go
+++ 14-generics.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+type kus struct {
+	cins    string
+	renk    string
+	habitat string
+	yas     int
+}
+
+type kopek struct {
+	cins     string
+	renk     string
+	yas      string
+	safkanMi bool
+}
+
+type hayvan[T kus | kopek] struct {
+	bilgi T
+	isim  string
+}
+
 /*
 	Go'da Generics (Tür Parametreleri):
 		- Fonksiyonlar ve veri yapıları için tür parametreleri tanımlamaya olanak tanır
@@ -14,6 +33,12 @@ func turParametreleri() {
 	floatDilimi := []float64{1.5, 2.5, 3.5}
 	fmt.Println("🔢 int dilimi toplamı:", sliceTopla(intDilimi))
 	fmt.Println("🔢 float dilimi toplamı:", sliceTopla(floatDilimi))
+
+	kusum := hayvan[kus]{bilgi: kus{cins: "Serçe", renk: "Kahverengi", habitat: "Orman", yas: 2}, isim: "Cici"}
+	kopegim := hayvan[kopek]{bilgi: kopek{cins: "Golden Retriever", renk: "Altın", yas: "3", safkanMi: true}, isim: "Gofret"}
+
+	fmt.Printf("🐦 Hayvan türü: %v, Cinsi: %v, İsmi: %v\n", "Kuş", kusum.bilgi.cins, kusum.isim)
+	fmt.Printf("🐶 Hayvan türü: %v, Cinsi: %v, İsmi: %v\n", "Köpek", kopegim.bilgi.cins, kopegim.isim)
 }
 
 func sliceTopla[T int | float64](dilim []T) T {
